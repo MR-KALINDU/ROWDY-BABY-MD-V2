5b53eac14513ab6c667d3a2f292723f1b2a11f90
KALIDUWA OFC ✔️ <132440655+MR-KALINDU@users.noreply.github.com>
2023-09-06 19:28:04 +0530

    💠 ʀᴏᴡᴅʏ-ʙᴀʙʏ-ᴍᴅ 💠

--- commands/converter.js
+++ commands/converter.js
@@ -1,4 +1,10 @@
+const axios = require('axios')
+const { sck1, tiny, fancytext, listall,cmd,ffmpeg } = require('../lib/')
+const fs = require('fs-extra');
+const { exec } = require('child_process')
+const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter");
 
+    //---------------------------------------------------------------------------
 cmd({
             pattern: "photo",
             desc: "Makes photo of replied sticker.",
