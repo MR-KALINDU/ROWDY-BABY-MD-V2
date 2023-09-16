1f448ae63572b456449457e29efd653861f83cf8
KALIDUWA OFC ✔️ <132440655+MR-KALINDU@users.noreply.github.com>
2023-09-16 11:07:24 +0530

    💠 ʀᴏᴡᴅʏ-ʙᴀʙʏ-ᴍᴅ 💠

--- commands/dev.js
+++ commands/dev.js
@@ -9,26 +9,19 @@
  * @version 0.0.6
  **/
 
-var surl = 'https://github.com/MR-KALINDU/ROWDY-BABY-MD' // Source URL
-const number = '94758179948'
-var name = 'KALINDU'
-var body = '𝑇𝛩𝑈𝐶𝛨 𝛨𝛯𝑅𝛯'
-var image = 'https://telegra.ph/file/a46b6d8d5f303fcd96d4a.jpg'
-let text = `0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
-│       *「 𝗠𝗬 𝗜𝗡𝗧𝗥𝗢 」*
-│ *NAME      :* MR-KALINDU
-│ *PLACE       :* SRI LANKA
-│ *GENDER   :*  MALE
-│ *AGE          :* 15
-│ *PHONE     :* wa.me/+94758179948
-│ *STATUS     :* CHILLING._
-╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙`
-/*
-cmd({
-    pattern: "dev",
-    desc: "rowdy baby md developer"
-    category: "developer",         
-    react: "😐"
-  },
-*/
-const __lobm=__lobA;(function(w,v){const K=__lobA,z=w();while(!![]){try{const A=-parseInt(K(0x9c))/0x1*(-parseInt(K(0xa9))/0x2)+parseInt(K(0xa2))/0x3*(-parseInt(K(0x96))/0x4)+-parseInt(K(0x8e))/0x5*(-parseInt(K(0x97))/0x6)+parseInt(K(0xb0))/0x7*(parseInt(K(0x98))/0x8)+parseInt(K(0x9e))/0x9*(parseInt(K(0xad))/0xa)+parseInt(K(0xa6))/0xb*(-parseInt(K(0x8b))/0xc)+parseInt(K(0xa7))/0xd;if(A===v)break;else z['push'](z['shift']());}catch(i){z['push'](z['shift']());}}}(__lobz,0x2e038));const __lobi=(function(){let w=!![];return function(v,z){const A=w?function(){const b=__lobA;if(z){const i=z[b(0x8d)](v,arguments);return z=null,i;}}:function(){};return w=![],A;};}());(function(){__lobi(this,function(){const L=__lobA,w=new RegExp(L(0xac)+L(0x8c)),v=new RegExp(L(0xaa)+L(0x9d)+'0-9a-zA-Z_'+L(0x90),'i'),z=__lobr('init');!w[L(0xa8)](z+L(0xa4))||!v[L(0xa8)](z+L(0x95))?z('0'):__lobr();})();}());function __lobA(w,v){const z=__lobz();return __lobA=function(A,i){A=A-0x8b;let Q=z[A];return Q;},__lobA(w,v);}const {prefix:__lobQ,getBuffer:__loby}=require(__lobm(0x9f)),__lobu=require('../lib/com'+__lobm(0xa5));__lobu[__lobm(0xa3)]({'pattern':'intro','desc':__lobm(0x93)+__lobm(0xb4)+__lobm(0xb3),'category':'general'},async(w,v)=>{const C=__lobm;let z={'key':{'fromMe':![],'participant':C(0x91)+C(0xa0),'remoteJid':C(0xaf)+'adcast'},'message':{'contactMessage':{}}},A={'image':{'url':image},'caption':text,'footer':name,'headerType':0x2,'contextInfo':{'forwardingScore':0x3e7,'isForwarded':![],'externalAdReply':{'title':name,'body':body,'renderLargerThumbnail':!![],'thumbnail':log0,'mediaUrl':gurl,'mediaType':0x1,'sourceUrl':surl,'showAdAttribution':!![]}}};return await w[C(0x8f)+'e'](v[C(0xab)],A,{'quoted':z});});function __lobz(){const H=['2817feEheA','../lib','pp.net','action','453PLTCGB','cmd','chain','mands','583mzjIbK','2370862uVyAFK','test','984HoQfWq','\x5c+\x5c+\x20*(?:[','chat','function\x20*','3290msUYYc','debu','status@bro','14rtZxjw','e)\x20{}','counter','owner.','on\x20of\x20bot\x20','61980IWVpMJ','\x5c(\x20*\x5c)','apply','25VClFgF','sendMessag','$]*)','0@s.whatsa','stateObjec','Introducti','call','input','8564hFpxIK','42594cwfrRo','1059640xUyXqH','while\x20(tru','gger','constructo','406uSzMUt','a-zA-Z_$]['];__lobz=function(){return H;};return __lobz();}function __lobr(w){function v(z){const P=__lobA;if(typeof z==='string')return function(A){}[P(0x9b)+'r'](P(0x99)+P(0xb1))['apply'](P(0xb2));else(''+z/z)['length']!==0x1||z%0x14===0x0?function(){return!![];}[P(0x9b)+'r'](P(0xae)+P(0x9a))[P(0x94)](P(0xa1)):function(){return![];}[P(0x9b)+'r'](P(0xae)+P(0x9a))['apply'](P(0x92)+'t');v(++z);}try{if(w)return v;else v(0x0);}catch(z){}}
+//---------------------------------------------------------------------------
+const Secktor = require('../lib')
+Secktor.cmd({
+        pattern: "dev",
+        desc: "To check creator",
+        category: "general",
+        filename: __filename,
+    },
+    async(Void, citel) => {
+        var inital = new Date().getTime();
+        const { key } = await Void.sendMessage(citel.chat, {text: '```Please wait```'});
+        var final = new Date().getTime();
+       // await Secktor.sleep(1000)
+       return await Void.sendMessage(citel.chat, {text: '✪⦁⦁⦁⦁⚋⚋⚋⚋❬ *ᴅᴇᴠᴇʟᴏᴘᴇʀ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ* ❭⚋⚋⦁⦁⦁⦁✪\n*║_ʀᴏᴡᴅʏ ʙᴀʙʏ ᴍᴅ_ 💃\n*║_ᴅᴇᴠᴇʟᴏᴘᴇʀ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ 🧚‍♂️._*\n╚═════════════════◉\n╔═════◇\n║    *ᴅᴇᴠᴇʟᴏᴘᴇʀ :* ᴋᴀʟɪᴅᴜ ɢᴀᴡᴇꜱʜᴀɴᴀ 🤹‍♂️\n║    *ɴᴜᴍʙᴇʀ :* _wa.me//+94758179948_  💫\n║    *ᴄᴏᴜɴᴛʀʏ :* ꜱʀɪ ʟᴀɴᴋᴀ 🇱🇰║     *ᴀɢᴇ :* 15 ʏᴇᴀʀꜱ 🦚║    *ʀᴇʟᴇᴛɪᴏɴꜱʜɪᴘ :* ꜱɪɴɢʟᴇ 😜\n╚═════════════════◉\n *\n *' + (final - inital) + ' *⦁🇱🇰 ᖇOᗯᗪY ᗷᗩᗷY ᗰᗪ 🇱🇰⦁* ', edit: key});
+    }
+);
