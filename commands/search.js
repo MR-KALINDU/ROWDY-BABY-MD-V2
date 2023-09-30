42f1e2e2a7f72f06751d6f023cd77ccf2ca3d5d0
KALIDUWA OFC ✔️ <132440655+MR-KALINDU@users.noreply.github.com>
2023-09-30 07:13:39 +0530

    💠 ʀᴏᴡᴅʏ-ʙᴀʙʏ-ᴍᴅ 💠

--- commands/search.js
+++ commands/search.js
@@ -211,40 +211,7 @@ cmd({
     )
    
     //---------------------------------------------------------------------------
-cmd({
-            pattern: "image",
-            category: "search",
-            desc: "Searches Image on Google",
-            use: '<text>',
-            filename: __filename,
-        },
-        async(Void, citel, text) => {
-            if (!text) return citel.reply("Provide me a query!")
-            if (!text) return reply("Hey bie please tell me for which pic you're looking");
-            let name1 = text.split("|")[0]
-            let name2 = text.split("|")[1] || `1`
-            citel.reply(`Sending ${name2} image(s) of ${name1} in chat`)
-            let nn = name2
-            for (let i = 0; i < nn; i++) {
 
-                let n = await gis(name1)
-                images = n[Math.floor(Math.random() * n.length)].url;
-                    let buttonMessage = {
-                        image: {
-                            url: images,
-                        },
-                        caption: `_👮‍♀️ ʀᴏᴡᴅʏ ʙᴀʙʏ ɪᴍᴀɢᴇ ꜱᴇᴀʀᴄʜ 👮‍♀️_\n*${name1}*`,
-                        headerType: 4,
-                    };
-                    Void.sendMessage(citel.chat, buttonMessage, {
-                        quoted: citel,
-                    });
-            }
-        }
-    )
-    
-
-//---------------------------------------------------------------------------
 
 cmd({
             pattern: "couplepp",
