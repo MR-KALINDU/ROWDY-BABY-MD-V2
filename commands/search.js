916fe2f4731e165f7b42e9e2c006eb726ef39c5a
KALIDUWA OFC ✔️ <132440655+MR-KALINDU@users.noreply.github.com>
2023-09-30 08:21:03 +0530

    💠 ʀᴏᴡᴅʏ-ʙᴀʙʏ-ᴍᴅ 💠

--- commands/search.js
+++ commands/search.js
@@ -6,36 +6,89 @@ const fetch = require('node-fetch')
 //---------------------------------------------------------------------------
 cmd({
             pattern: "image",
-            alias :['img'],
+            alias: ["img" , "pic"],
             category: "search",
             desc: "Searches Image on Google",
             use: '<text>',
             filename: __filename,
         },
         async(Void, citel, text) => {
-            if (!text) return citel.reply("Provide me a query!")
-            if (!text) return reply("Hey bie please tell me for which pic you're looking");
-            let name1 = text.split("|")[0]
-            let name2 = text.split("|")[5] || `5`
-            citel.reply(`*🛸 Sending ${name2} image(s) of ${name1} in chat 🛸*`)
+
+   if (!text) return citel.reply(`Provide me a query!\n*Ex : .image luffy |10*`)
+   let buttonMessage = {}
+   let name1 = text.split("|")[0] || `Luffy`
+   let name2 = text.split("|")[1] || `5`
+ try {
+    let urlsArray = [];
+    const params = {
+        q: name1, 
+        tbm: "isch",
+        hl: "en",
+        gl: "in",
+        ijn: "0", 
+    };
+    const headers = {
+      "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
+      "Accept-Encoding": "application/json",
+  };
+  
+    const res = await axios.get("https://www.google.com/search", { headers: headers, params: params });
+    let body = res.data;
+    body = body.slice(body.lastIndexOf("AF_initDataCallback"));
+    body = body.slice(body.indexOf("["));
+    body = body.slice(0, body.indexOf("</script>")-1);
+    body = body.slice(0, body.lastIndexOf(","));
+    
+    const img = JSON.parse(body);
+
+    const imgObjects = img[56][1][0][0][1][0];
+    for (let i = 0; i < name2; i++) {
+        if (imgObjects[i] && imgObjects[i][0][0]["444383007"][1]) {
+            let url = imgObjects[i][0][0]["444383007"][1][3][0]; // the url
+            urlsArray.push(url);
+        }
+    }
+
+for (let url of urlsArray) { Void.sendMessage(citel.chat , {image : {url : url} } )  }
+} 
+ catch (error) {   return citel.reply("*Google Images Not Working, Try it Later*"); }
+
+ 
+ 
+ /*
+ 
+let isImages = false;
+            let num = text.split("|")[1];
+ gis(name1, async(error, result) => { 
+if(result.length) 
+{
+ isImages = true;
+ citel.reply(`Sending images of ${name1} in chat`) 
+}
+else return citel.reply("*Google Images Not Working, Try it Later*");
+})
+     if(!isImages) return       
             let nn = name2
             for (let i = 0; i < nn; i++) {
-
-                let n = await gis(name1)
-                images = n[Math.floor(Math.random() * n.length)].url;
-                    let buttonMessage = {
-                        image: {
-                            url: images,
-                        },
-                        caption: `*♣️ _ʀᴏᴡᴅʏ ʙᴀʙʏ ɪᴍᴀɢᴇ ꜱᴇᴀʀᴄʜ_ ♣️*\n*${name1}*`,
-                        headerType: 4,
-                    };
-                    Void.sendMessage(citel.chat, buttonMessage, {
-                        quoted: citel,
-                    });
+            gis(name1, async(error, result) => { 
+            n = result;
+            images = n[Math.floor(Math.random() * n.length)].url;
+            
+             
+             if(!num){ buttonMessage = {   image: { url: images },
+                                caption: Config.caption,
+                                }
+             }else {  buttonMessage = {   image: { url: images },}   }
+                    
+             
+             Void.sendMessage(citel.chat, buttonMessage, { quoted: citel });
+                })
             }
-        }
-    )   
+            
+            
+            */
+ })
 //---------------------------------------------------------------------------
    const { shazam } = require('../lib')
    let yts = require("secktor-pack");
